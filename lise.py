a2766466d3ee70769b12a582fe1e19b9a55cc823
Sahanuj <82720442+Sahanuj@users.noreply.github.com>
2021-11-25 17:35:13 +0530

    Update lise.py
    logo

--- lise.py
+++ lise.py
@@ -7,8 +7,22 @@ def clear():
     if "linux" in sys.platform.lower():os.system("clear")
     elif "win" in sys.platform.lower():os.system("cls")
     else:os.system("clear")
-def banner():
-        print ('\x1b[1;91m         ____ _____ __  __  ___   ___\n\x1b[1;91m        |  _ \\___ /|  \\/  |/ _ \\ / _ \\\n\x1b[1;92m        | | | ||_ \\| |\\/| | | | | (_) |\n\x1b[1;92m        | |_| |__) | |  | | |_| |\\__, |\n\x1b[1;91m        |____/____/|_|  |_|\\___/   / /\n\x1b[1;91m                                  / /\n\x1b[1;97m-------------------------------------------------')
+##logo
+_logo_line_1_ = ('\x1b[1;91m___   ___ ___ ___ _______ _______')
+_logo_line_2_ = ('\x1b[1;91m| |   | | | | | | |  ___| |_   _|')
+_logo_line_3_ = ('\x1b[1;92m| |___| | | | | | | |___    | |  ')
+_logo_line_4_ = ('\x1b[1;92m|  ___  | | | | | |___  |   | |  ')
+_logo_line_5_ = ('\x1b[1;91m| |   | | | |_| | ____| |  _| |  ')
+_logo_line_6_ = ('\x1b[1;91m|_|   |_| |_____| |_____| |___|  ')
+_logo_line_7_ = ('\x1b[1;97m-------------------------------------------------')
+def _my_logo_():
+    print(_logo_line_1_)
+    print(_logo_line_2_)
+    print(_logo_line_3_)
+    print(_logo_line_4_)
+    print(_logo_line_5_)
+    print(_logo_line_6_)
+    print(_logo_line_7_+'\n')
 
 def spt():
 	try:
@@ -26,7 +40,7 @@ def spt():
 		##user1()
 def user():
     clear()
-    banner()
+    _my_logo_()
     print(45*"_")
     print ('  [+]Generating ID ...');time.sleep(3)
     print ("  [+]SUCCES");time.sleep(0.07)
@@ -46,7 +60,7 @@ def user():
 def user1():
     try:
       clear()
-      banner()
+      _my_logo_()
       j = input(" [+] Enter Your License key(must be 25 characters: ")
       r = requests.get("https://github.com/Sahanuj/Ujbf/blob/main/Uj/li.txt").text
       if len(j)!=25:print("key must be 25 characters").exit()
@@ -54,13 +68,13 @@ def user1():
       if j in r:
           print("  Please Wait .. Checking Your Key !");time.sleep(3)
           clear()
-          banner()
+          _my_logo_()
           print("  Login Status : \x1b[92mComplete\x1b[0m")
           time.sleep(1)
           os.sys.exit(elite1.menu())
       else:
           clear()
-          banner()
+          _my_logo_()
           print ("  [!]Login Status :\x1b[91m Failed \x1b[0m");time.sleep(0.07)
           print ("  [+]ID Not Confirmed");time.sleep(0.07)
           print ("  [+]Please Chat Admin For Confirmed Your ID");time.sleep(0.07)
